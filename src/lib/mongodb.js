bb272289ed84db0f4e5740d033be91001692b804
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2026-01-23 19:09:37 -0500

    update files

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,21 +1,24 @@
-import { MongoClient } from "mongodb";
+// /lib/mongodb.js
+import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
+if (!uri) {
+  throw new Error("Please add your Mongo URI to environment variables");
 }
 
 let client;
 let clientPromise;
 
 if (process.env.NODE_ENV === "development") {
+  // In dev, use global to avoid multiple connections
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
+  // In production, create new client
   client = new MongoClient(uri);
   clientPromise = client.connect();
 }
